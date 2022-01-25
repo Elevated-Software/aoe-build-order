33dbddbfa163157435235d2b23bd1a46d0db049d
Brennen Davis <brennendavis@gmail.com>
2022-01-25 00:08:17 -0700

    get a single build order

--- src/pages/api/build-orders/[id].ts
+++ src/pages/api/build-orders/[id].ts
@@ -5,21 +5,22 @@ import { EsApiResponse } from '../../../lib/models/api';
 import { BuildOrder, IBoLineItemDoc, IBuildOrderDoc } from '../../../lib/models/database';
 
 interface Data {
-  buildOrders: LeanDocument<IBuildOrderDoc>[];
+  buildOrders: LeanDocument<IBuildOrderDoc>;
 };
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: EsApiResponse<Data>) => {
+  const { id: boId } = req.query;
   switch (req.method) {
     case 'GET': {
-      const buildOrders = await get();
+      const buildOrders = await get(boId as string);
       res.json({ success: true, buildOrders });
     }
     default: break;
   }
 };
 
-const get = async () => {
-  return await BuildOrder.find({}).populate<{ lineItems: IBoLineItemDoc[]; }>('lineItems').lean().exec();
+const get = async (boId: string) => {
+  return await BuildOrder.findById(boId).populate<{ lineItems: IBoLineItemDoc[]; }>('lineItems').lean().exec();
 };
 
 export default withHandleErrors(withDb(handler));
