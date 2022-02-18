8b60271c6050dc7751a6577e3d157d8fca181d49
Brennen Davis <brennendavis@gmail.com>
2022-02-18 00:30:35 -0700

    filter grid shows correctly on different screen sizes

---
+++ src/pages/api/build-orders/search.ts
@@ -0,0 +1,19 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { withDb, withHandleErrors } from '../../../lib/middlewares';
+import { BuildOrder, IBoStepDoc, IBuildOrderDoc, IUserDoc } from '../../../lib/models/database';
+
+type Data = {
+  searchBuildOrders: IBuildOrderDoc | null;
+};
+
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const buildOrder = await BuildOrder.findOne({ name: 'Test Build Order' }).exec();
+  console.log(`from not populated ${buildOrder?.user}`);
+
+  const populatedBuildOrder = await BuildOrder.findOne({ name: 'Test Build Order' }).populate<{ user: IUserDoc; steps: IBoStepDoc[]; }>('user steps').exec();
+  console.log(`from populated ${populatedBuildOrder.user.email}`);
+
+  res.status(200).json({ bo: populatedBuildOrder });
+};
+
+export default withHandleErrors(withDb(handler));
--- src/pages/build-orders/index.tsx
+++ src/pages/build-orders/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Grid, GridItem, Heading, Spinner, Text } from '@chakra-ui/react';
+import { Center, Grid, GridItem, Heading, Spinner, Text, useBreakpoint } from '@chakra-ui/react';
 import useSWR from 'swr';
 import { BuildOrderFilter } from '../../components/build-orders/BuildOrderFilter';
 import { BuildOrderList } from '../../components/build-orders/BuildOrderList';
@@ -8,13 +8,22 @@ import { Bo } from '../../lib/models/api';
 
 const BuildOrders = (): JSX.Element => {
   const { data, error } = useSWR<{ success: boolean, buildOrders: Bo[]; }>('/api/build-orders');
+  const breakpoint = useBreakpoint();
+  const smallScreen = breakpoint === 'base' || breakpoint === 'sm';
+  console.log(breakpoint);
 
   return (
     <Container>
       <Grid w={{ base: '90%', md: '60%' }} templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }} gap={20}>
-        <GridItem colSpan={1}>
-          <BuildOrderFilter />
-        </GridItem>
+        {
+          smallScreen
+            ? (
+              <GridItem colSpan={1}>
+                <BuildOrderFilter />
+              </GridItem>
+            )
+            : null
+        }
         <GridItem colSpan={2}>
           <Heading pb={4} size="xl">Build Orders</Heading>
           {
@@ -23,6 +32,15 @@ const BuildOrders = (): JSX.Element => {
               : <Center><Spinner /></Center>
           }
         </GridItem>
+        {
+          !smallScreen
+            ? (
+              <GridItem colSpan={1}>
+                <BuildOrderFilter />
+              </GridItem>
+            )
+            : null
+        }
       </Grid>
     </Container>
   );
