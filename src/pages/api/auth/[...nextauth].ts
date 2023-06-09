32fe7276d5b8b485b3afaa1a89fa98962cf3d21c
Brennen Davis <brennendavis@gmail.com>
2023-06-08 22:37:42 -0600

    remove comments

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import mongoose from 'mongoose';
-import { dbConnect } from '../../../lib/middlewares/withDb';
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import mongoose from "mongoose";
+import { dbConnect } from "../../../lib/middlewares/withDb";
 
 // Make sure the db is connected before a user tries to login
 (async () => {
@@ -11,37 +11,33 @@ import { dbConnect } from '../../../lib/middlewares/withDb';
 
 export default NextAuth({
   pages: {
-    signIn: '/auth/signin',
-    verifyRequest: '/auth/verify-request',
+    signIn: "/auth/signin",
+    verifyRequest: "/auth/verify-request",
   },
   providers: [
-    // EmailProvider({
-    //   server: process.env.SMTP_SERVER,
-    //   from: process.env.FROM_EMAIL,
-    //   // Email is valid for 2hrs
-    //   maxAge: 60 * 60 * 2,
-    // }),
     EmailProvider({
       server: {
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        name: 'mail.elevatedsoftware.dev',
+        name: "mail.elevatedsoftware.dev",
         auth: {
           user: process.env.SMTP_EMAIL_USER,
-          pass: process.env.SMTP_EMAIL_PW
-        }
+          pass: process.env.SMTP_EMAIL_PW,
+        },
       },
       from: process.env.STMP_EMAIL_FROM,
       // Email is valid for 2hrs
       maxAge: 60 * 60 * 2,
     }),
   ],
-  adapter: MongoDBAdapter(new Promise((resolve, _) => resolve(mongoose.connection.getClient() as any))),
+  adapter: MongoDBAdapter(
+    new Promise((resolve, _) => resolve(mongoose.connection.getClient() as any))
+  ),
   secret: process.env.EMAIL_SECRET,
   callbacks: {
     session({ session, user }) {
       session.user.userId = user.id;
       return session;
     },
-  }
+  },
 });
